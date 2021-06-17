a7b6f73d6a32cbd43f5f371bc176bfb1b14c13b2
Jorge Maidana <jorgem.seq@gmail.com>
2021-06-17 09:16:08 -0300

    Switch from 'pci_' to 'dma_' API
    Signed-off-by: Jorge Maidana <jorgem.seq@gmail.com>

--- src/cx88_sdr_core.c
+++ src/cx88_sdr_core.c
@@ -226,7 +226,7 @@ static int cx88sdr_probe(struct pci_dev *pdev,
 
 	pci_set_master(pdev);
 
-	if (pci_set_dma_mask(pdev, DMA_BIT_MASK(32))) {
+	if (dma_set_mask(&pdev->dev, DMA_BIT_MASK(32))) {
 		dev_err(&pdev->dev, "no suitable DMA support available\n");
 		ret = -EFAULT;
 		goto disable_device;
